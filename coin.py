463f8f51ead275642477c97a2b9a49c22fa19de0
ujjwalsb <ujjwalsingh15@gmail.com>
2017-12-13 21:53:22 +0530

    half complete

--- coin.py
+++ coin.py
@@ -1,15 +1,23 @@
 import requests
-import csv
+import unicodecsv as csv
 from datetime import date
 
-r = requests.get('https://api.coinmarketcap.com/v1/ticker/?convert=INR&limit=2')
+r = requests.get('https://api.coinmarketcap.com/v1/ticker/?convert=INR&limit=3')
 res = (r.json())
 
-print type(res)
-print res
+# print res
 
-# today = date.today()
-# with open("BTC","w+") as f:
-# 	a = csv.DictWriter (f, res.keys())
-# 	a.writeheader
-# 	a.writerow(res)
+dict_keys = res[0].values()
+keys = res[0].keys()
+today = date.today()
+
+# print type(dict_keys)
+
+for dict_keys[7] in enumerate(dict_keys[8]): 
+
+	with open(str(dict_keys[7])+"_"+str(today)+".csv","w+") as f:
+		a = csv.DictWriter (f, keys)
+		a.writeheader()
+		a.writerows(res)
+
+		print a.writeheader
